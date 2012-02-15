7b03c96772ffa406e0efb7343e28ca73d3026684
Chris Hoffman <choffman@beechstcap.com>
2012-02-15 12:22:28 -0500

    Debugging

--- lib/qualified_has_and_belongs_to_many/associations.rb
+++ lib/qualified_has_and_belongs_to_many/associations.rb
@@ -11,6 +11,7 @@ module ActiveRecord::Associations
 
   module ClassMethods
     def qualified_has_and_belongs_to_many(name, qualifier, options = {}, &extension)
+      debugger
       Builder::QualifiedHasAndBelongsToMany.build(self, name, qualifier, options, &extension)
     end
   end
